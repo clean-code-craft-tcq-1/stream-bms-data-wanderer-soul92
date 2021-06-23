233764329dc831714c8b8c10b1dc326d96be1df7
NaveenBalachandar-dev <77613377+NaveenBalachandar-dev@users.noreply.github.com>
2021-06-23 11:45:36 +0530

    Create BMS_Receiver.h

---
+++ BMS_Receiver/BMS_Receiver.h
@@ -0,0 +1,8 @@
+/*--------------------------------------------------------------------------------
+---    \file       BMS_Receiver.h
+---   \author      Naveen Balachandar (RBEI/ECK3) 
+---
+---     \brief     proj header file to recive and process stream data
+------------------------------------------------------------------------------------*/
+/*Func declarations*/
+void receiveConsoleData_v(void);
