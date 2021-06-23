08f7db5887c3ef16d720614fb993cf4a7cb4b328
NaveenBalachandar-dev <77613377+NaveenBalachandar-dev@users.noreply.github.com>
2021-06-23 13:07:13 +0530

    Update BMS_Receiver.c

--- BMS_Receiver/BMS_Receiver.c
+++ BMS_Receiver/BMS_Receiver.c
@@ -13,19 +13,28 @@
 /*------ Project includes -------*/
 #include "BMS_Receiver.h"
 
+/*---------------------------------------------------------------------------*/
+/*     FUNCTION:    receiveConsoleData_v
+ */
+/*!    \brief       Func to rx and decode from console
+ * 
+ *     \param       None
+ *     \returns     None
+ *
+*//*------------------------------------------------------------------------*/
+
 void receiveConsoleData_v()
 {
   char rxBuffer[1000] = {0};
   int i;
   char *ret;
-   //char *ret2;
   int start =15;
   int len =2;
   char destination[25];
-  static int Temp[30] = {0};
+  int Temp[30] = {0};
   int soc[30] = {0};
   int chargerate[30] = {0};
-  char tempbuffer[30];
+  //char tempbuffer[30];
   int index =0;
   int index1 =0;
   int index2 =0;
@@ -42,14 +51,12 @@ void receiveConsoleData_v()
   ret = strstr(rxBuffer, "\"temperature\": ");
   if (ret != NULL)
   {
-   //printf("rx temp data is %s\n", ret);
    findingsubstring_p(destination, ret, start, len);
-   //ret2 = strtok(ret, ": ");
    //printf("rx temp1 data is %s\n", destination);
      Temp[index] = atoi(destination);
      printf("rx temp2 data is %d\n", Temp[index]);
     
-     printf("rx index data is %d\n", index);
+     //printf("rx index data is %d\n", index);
      index = index + 1;
   }
     
@@ -61,7 +68,6 @@ void receiveConsoleData_v()
    start =7;
    //printf("rx temp data is %s\n", ret);
    findingsubstring_p(destination, ret, start, len);
-   //ret2 = strtok(ret, ": ");
    //printf("rx soc data is %s\n", destination);
     soc[index1++] = atoi(destination);
   }  
@@ -74,26 +80,31 @@ void receiveConsoleData_v()
    start =15;
    //printf("rx temp data is %s\n", ret);
    findingsubstring_p(destination, ret, start, len);
-   //ret2 = strtok(ret, ": ");
    //printf("rx charge rate data is %s\n", destination);
     chargerate[index2++]= atoi(destination);
   } 
     
-  /*Finding min and max of temperature*/
-    /* Assume first element as maximum and minimum */
-  
-     
-    
-    
-   //ret2 = strtok(rxBuffer, ": ");
+ 
   }
+  
+   /*Finding min and max of temperature*/
+    /* Assume first element as maximum and minimum */
    max = Temp[0];
      min = Temp[0];
-    computeMinMax(Temp, &max ,&min);
-
+    computeMinMax_v(Temp, &max ,&min);
+    printf("rx temp data Min:%dMax:%d",min,max);
 }
 
-// Function to implement substring function in C
+/*---------------------------------------------------------------------------*/
+/*     FUNCTION:    findingsubstring_p
+ */
+/*!    \brief       func to find reqd substring in comeplete string
+ * 
+ *     \param       destbuffer ,sourcebuffer ,start index and len
+ *     \returns     None
+ *
+*//*------------------------------------------------------------------------*/
+
 char* findingsubstring_p (char *dest, const char *src, int stIndx, int len)
 {
     // extracts `n` characters from the source string starting from `beg` index
@@ -114,36 +125,56 @@ char* findingsubstring_p (char *dest, const char *src, int stIndx, int len)
     return dest;
 }
 
-void  computeMinMax(int currentVal_a[], int *maxValue, int *minValue)
-{
-  int i;
-  
-   for(i=1; i<15; i++)
-    {
-        /* If current element is greater than max */
-      if(currentVal_a[i] > *maxValue)
-      {
-       *maxValue = currentVal_a[i];
-       }
+/*---------------------------------------------------------------------------*/
+/*     FUNCTION:    computeMinMax
+ */
+/*!    \brief       compute Min and Max value in recived buffer
+ * 
+ *     \param       rxArray ,Max value and Min value
+ *     \returns     None
+ *
+*//*------------------------------------------------------------------------*/
 
-       /* If current element is smaller than min */
-       if(currentVal_a[i] < *minValue)
-       {
-        *minValue = currentVal_a[i];
+void  computeMinMax_v (int currentVal_a[], int *maxValue, int *minValue)
+{
+    int idx;
+   
+    for(idx=1; idx<15; idx++) /*checking complete array*/
+    {
+        /* If current element in array is greater than max */
+        if(currentVal_a[i] > *maxValue)
+        {
+           *maxValue = currentVal_a[i];
+        }
+
+        /* If current element in array is smaller than min */
+        if(currentVal_a[i] < *minValue)
+        {
+            *minValue = currentVal_a[i];
         }
      }
-   printf("rx temp data Min:%dMax:%d",*minValue,*maxValue);
 }
-void  computeAverage(int currentValue[], int NumofValues)
+
+/*---------------------------------------------------------------------------*/
+/*     FUNCTION:    computeAverage_v
+ */
+/*!    \brief       compute Min and Max value in recived buffer
+ * 
+ *     \param       rxArray and  num of values
+ *     \returns     None
+ *
+*//*------------------------------------------------------------------------*/
+
+void  computeAverage_v(int currentValue[], int NumofValues)
 {
 
 
 }
+
 int  main()
 {
- // printf("Receiver data processing");
-  receiveConsoleData_v();
-  return 0;
-  
+    /*Func to rx and decode from console*/
+    receiveConsoleData_v();
+    return 0;  
 }
 
