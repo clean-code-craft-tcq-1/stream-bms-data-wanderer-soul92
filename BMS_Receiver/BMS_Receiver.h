70397ab2be47bf078542138f5bbffdc7f33f08d2
NaveenBalachandar-dev <77613377+NaveenBalachandar-dev@users.noreply.github.com>
2021-06-23 12:24:03 +0530

    Update BMS_Receiver.h
    improved header

--- BMS_Receiver/BMS_Receiver.h
+++ BMS_Receiver/BMS_Receiver.h
@@ -4,5 +4,9 @@
 ---
 ---     \brief     proj header file to recive and process stream data
 ------------------------------------------------------------------------------------*/
+
 /*Func declarations*/
 void receiveConsoleData_v(void);
+char* findingsubstring_p (char *dest, const char *src, int stIndx, int len);
+void  computeMinMax(int currentValue[], int *maxValue, int *minValue);
+void  computeAverage(int currentValue[], int NumofValues);
